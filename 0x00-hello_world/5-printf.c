999b237612ed742ad2c8d342754ae45621ba3e02
Anonymous guy <icedeveloper247@gmail.com>
2023-07-13 10:12:23 +0100

    Task Completed

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main - Prints a line of text using the printf() funtion
+ *
+ * Return: 0 (always)
+ *
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+return (0);
+}
