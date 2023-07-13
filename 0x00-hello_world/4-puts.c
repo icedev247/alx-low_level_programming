675527d64ce3d234da39d6c95f6ef3ee5d35b51c
Anonymous guy <icedeveloper247@gmail.com>
2023-07-13 10:07:38 +0100

    Task Completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /*
- * main - Entry point
+ * main - Program prints a line of text using the puts() function
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
