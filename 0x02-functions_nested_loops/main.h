4caacbfcbb58bd0c904c72c1da00f2b210993fde
Anonymous guy <icedeveloper247@gmail.com>
2023-07-18 20:40:21 +0100

    Done

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef main_h
+#define main_h
+/**
+ * _putchar -  prints character input
+ *
+ * @i: Description of parameter i
+ *
+ * return: value
+ *
+ */
+void _putchar(int i)
+{
+	return (putchar(i));
+}
+#endif
