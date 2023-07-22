443ffacd7508a37d49be7b5f0fd8df21ef80aa73
Anonymous guy <icedeveloper247@gmail.com>
2023-07-22 17:46:08 +0100

    Done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -18,11 +18,11 @@ int main(void)
 	unsigned long int aft1;
 	unsigned long int aft2;
 
-	printf("%lu ", bef);
+	printf("%lu, ", bef);
 
 	for (i = 1; i < 91; i++)
 	{
-		printf(",%lu ", aft);
+		printf("%lu, ", aft);
 		aft += bef;
 		bef = aft - bef;
 	}
@@ -34,8 +34,8 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %lu ", aft1 + (aft2 / l));
-		printf(", %lu ", aft2 % l);
+		printf("%lu, ", aft1 + (aft2 / l));
+		printf("%lu, ", aft2 % l);
 		aft1 += bef1;
 		bef1 = aft1 - bef1;
 		aft2 += bef2;
