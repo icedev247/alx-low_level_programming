f222154da2cea5deb39b7b4cc1e9a4f682efdd34
Anonymous guy <icedeveloper247@gmail.com>
2023-07-22 14:45:02 +0100

    Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes hte absolute value of an integer
+ * @r: Character to be printed
+ * Return: Always positive r
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
